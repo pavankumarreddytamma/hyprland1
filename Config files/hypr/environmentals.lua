71c10eefaed3c8165bf0025fe14018dc2cea1068
pavankumarreddytamma <2000090069csit@gmail.com>
2026-09-10 21:41:32 +0530

    removed unneccessary env

--- Config files/hypr/environmentals.lua
+++ Config files/hypr/environmentals.lua
@@ -5,8 +5,4 @@
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
-hl.env("LIBVA_DRIVER_NAME", "nvidia")
-hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
-hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 hl.env("AQ_DRM_DEVICES", "/dev/dri/card0:/dev/dri/card1")
-hl.env("GBM_BACKEND", "nvidia-drm")
