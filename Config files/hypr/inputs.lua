8714e5ea7c840672327a5bad94c623b14686919a
pavankumarreddytamma <2000090069csit@gmail.com>
2026-09-20 12:45:58 +0530

    added no accel profile for cursor

--- Config files/hypr/inputs.lua
+++ Config files/hypr/inputs.lua
@@ -12,8 +12,9 @@ hl.config({
 
         follow_mouse = 1,
 
-        sensitivity = -0.82, -- -1.0 - 1.0, 0 means no modification.
+        sensitivity = -0.77, -- -1.0 - 1.0, 0 means no modification.
         accel_profile = "flat",
+	force_no_accel = false, 
 
         touchpad = {
             natural_scroll = true,
