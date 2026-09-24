2a46a1f4469e5019df0abaec213031f7c3726aae
pavankumarreddytamma <2000090069csit@gmail.com>
2026-09-24 17:23:41 +0530

    changed the theme to pure black

--- Config files/hypr/inputs.lua
+++ Config files/hypr/inputs.lua
@@ -12,7 +12,7 @@ hl.config({
 
         follow_mouse = 1,
 
-        sensitivity = -0.72, -- -1.0 - 1.0, 0 means no modification.
+        sensitivity = -0.77, -- -1.0 - 1.0, 0 means no modification.
         accel_profile = "flat",
 
         touchpad = {
